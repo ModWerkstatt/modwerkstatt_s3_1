4c4ddf2037a85fa6fd664e3a6bf64964617e5256
Bandion-MW <norman.kroppen@googlemail.com>
2025-08-13 17:02:13 +0200

    initialisierung

---
+++ mod.lua
@@ -0,0 +1,25 @@
+﻿function data()
+return {
+	info = {
+		minorVersion = 0,
+		severityAdd = "NONE",
+		severityRemove = "WARNING",
+		name = _("mod_name"),
+		description = _("mod_desc"),
+		authors = {
+			{
+				name = "ModWerkstatt",
+				role = "CREATOR",
+			},
+		},
+		tags = { "europe", "locomotive", "kpev", "lokomotive", "länderbahn", "dampf", "steam",},
+		minGameVersion = 0,
+		dependencies = { },
+		url = { "" },
+        params = {
+        },
+	},
+	options = {
+	},
+}
+end
---
+++ strings.lua
@@ -0,0 +1,22 @@
+function data()
+return {
+	en = {
+
+		["mod_name"] = "S2 construction type Erfurt (Prussian)",
+		["mod_desc"] = "One year after the construction of the Hannover type, four more locomotives with the 2'B axle configuration were built by Henschel for the Erfurt branch, two of which were compound locomotives. Their heating surfaces were larger than those of the Hannover type. The Erfurt type S 2 was part of the 1891 test series of four express locomotives and four passenger locomotives (Prussian P 4.1), of which two were compound locomotives and two were twin locomotives. They were identical except for the modifications necessary for the compound drive. As with the Erfurt P 4, the trailing bogie did not prove particularly successful. In addition, the compound locomotives had design-related difficulties when starting up.",
+
+		["pr_s2_name"] = "Prussian S2 (Erfurt)",
+		["pr_s2_desc"] = "The S 2 class comprised express train locomotives of the Prussian State Railways. It included the types used by the Hanover, Erfurt, and Main-Neckar railway directorates.",
+
+	},
+	de = {
+
+		["mod_name"] = "S2 Bauart Erfurt (Preußen)",
+		["mod_desc"] = "Ein Jahr nach dem Bau der Bauart Hannover wurden vier weitere Maschinen mit der Achsformel 2'B für die Direktion Erfurt von Henschel hergestellt, davon zwei in Verbundbauart. Ihre Heizfläche war größer als die bei den Exemplaren der Bauart Hannover. Die S 2 der Bauart Erfurt gehörte zu der Versuchsserie von 1891 mit jeweils vier Schnellzug- und vier Personenzuglokomotiven (Preußische P 4.1), von denen je zwei in Verbund- und zwei in Zwillingsausführung gebaut wurden. Sie waren bis auf die für den Verbundantrieb notwendigen Änderungen baugleich. Wie auch bei der Erfurter P 4 bewährte sich auch hier das Laufdrehgestell nicht sonderlich. Außerdem hatte die Verbundlokomotiven bauartbedingte Schwierigkeiten bei der Anfahrt.",
+
+		["pr_s2_name"] = "Preußische S2 (Erfurt)",
+		["pr_s2_desc"] = "In der Gattung S 2 waren Schnellzuglokomotiven der Preußischen Staatseisenbahnen zusammengefasst. Es gab die Bauarten der Eisenbahndirektionen Hannover, Erfurt und Main-Neckar.",
+
+	},
+}
+end
